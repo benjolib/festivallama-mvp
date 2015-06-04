ec4e1dce4243dc8453e4c885dec3f03d2a76a4bf
Szabolcs Sztányi <sax155@icloud.com>
2015-06-05 01:26:54 +0200

    added new images

---
+++ Festivalama/Festivalama/viewControllers/FestivalDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  FestivalDetailViewController.h
+//  Festivalama
+//
+//  Created by Sztanyi Szabolcs on 04/06/15.
+//  Copyright (c) 2015 Sztanyi Szabolcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FestivalDetailViewController : UIViewController
+
+@end
