d7bcf26f4c60752d45159e7ace1bade3a1d816b6
Szabolcs Sztányi <sax155@icloud.com>
2015-06-10 00:51:21 +0200

    added new classes

---
+++ Festivalama/Festivalama/viewControllers/FestivalDetailBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  FestivalDetailBaseViewController.h
+//  Festivalama
+//
+//  Created by Sztanyi Szabolcs on 09/06/15.
+//  Copyright (c) 2015 Sztanyi Szabolcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FestivalDetailBaseViewController : UIViewController
+
+@end
---
+++ Festivalama/Festivalama/views/WebsiteButton.m
@@ -0,0 +1,21 @@
+//
+//  WebsiteButton.m
+//  Festivalama
+//
+//  Created by Sztanyi Szabolcs on 09/06/15.
+//  Copyright (c) 2015 Sztanyi Szabolcs. All rights reserved.
+//
+
+#import "WebsiteButton.h"
+
+@implementation WebsiteButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
