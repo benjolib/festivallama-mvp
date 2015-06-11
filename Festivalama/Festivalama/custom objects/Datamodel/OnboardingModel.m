ee9fea9b31784a2002b148ef2330af2806d41754
Szabolcs Sztányi <sax155@icloud.com>
2015-06-11 20:59:41 +0200

    fixed onboarding screens

--- Festivalama/Festivalama/custom objects/Datamodel/OnboardingModel.m
+++ Festivalama/Festivalama/custom objects/Datamodel/OnboardingModel.m
@@ -41,11 +41,11 @@
     NSArray *travelTitles = @[@"Selbe Stadt", @"Ganz Deutschland", @"Ganz Europa", @"Weltweit", @"Bin dabei, egal wohin"];
     [self fillArray:self.travelOptionsArray withItems:travelTitles];
 
-    NSArray *accompanyTitles = @[@"Zu zweit", @"3 bis 5 Freunde", @"5 bis 10 Freunde", @"Mehr als 10", @"Keine Ahnung"];
-    [self fillArray:self.friendsCountArray withItems:accompanyTitles];
-
     NSArray *overnightTitles = @[@"Zelt", @"Auto", @"Unter freiem Himmel", @"Wellness Hotel", @"Gar nicht, 3 Tage wach"];
     [self fillArray:self.overnightArray withItems:overnightTitles];
+
+    NSArray *accompanyTitles = @[@"Zu zweit", @"3 bis 5 Freunde", @"5 bis 10 Freunde", @"Mehr als 10", @"Keine Ahnung"];
+    [self fillArray:self.friendsCountArray withItems:accompanyTitles];
 }
 
 - (void)fillArray:(NSMutableArray*)array withItems:(NSArray*)itemsArray
@@ -67,10 +67,10 @@
             return self.travelOptionsArray;
             break;
         case 2:
-            return self.friendsCountArray;
+            return self.overnightArray;
             break;
         case 3:
-            return self.overnightArray;
+            return self.friendsCountArray;
             break;
         default:
             return nil;
@@ -88,10 +88,10 @@
             return @"Wie weit du für ein Festival reisen?";
             break;
         case 2:
-            return @"Mit wem reist du an?";
+            return @"Welche Übernachtung würdest du bevorzugen?";
             break;
         case 3:
-            return @"Welche Übernachtung würdest du bevorzugen?";
+            return @"Mit wem reist du an?";
             break;
         default:
             return nil;
