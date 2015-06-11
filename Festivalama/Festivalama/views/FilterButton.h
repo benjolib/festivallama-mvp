d5d3aafb9b27e74d9a73592c89c5a43636f4e82f
Szabolcs Sztányi <sax155@icloud.com>
2015-06-11 20:59:19 +0200

    added app icon

---
+++ Festivalama/Festivalama/views/FilterButton.h
@@ -0,0 +1,13 @@
+//
+//  FilterButton.h
+//  Festivalama
+//
+//  Created by Sztanyi Szabolcs on 11/06/15.
+//  Copyright (c) 2015 Sztanyi Szabolcs. All rights reserved.
+//
+
+#import "GreenButton.h"
+
+@interface FilterButton : GreenButton
+
+@end
---
+++ Festivalama/Festivalama/views/FilterButton.m
@@ -0,0 +1,13 @@
+//
+//  FilterButton.m
+//  Festivalama
+//
+//  Created by Sztanyi Szabolcs on 11/06/15.
+//  Copyright (c) 2015 Sztanyi Szabolcs. All rights reserved.
+//
+
+#import "FilterButton.h"
+
+@implementation FilterButton
+
+@end
