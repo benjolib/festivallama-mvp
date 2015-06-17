9cbd299ecdf4974a4849a342e40de9bb56a2ec8e
Szabolcs Sztányi <sax155@icloud.com>
2015-06-18 01:34:06 +0200

    added new fonts

---
+++ Festivalama/Festivalama/custom objects/UIFont+LatoFonts.h
@@ -0,0 +1,13 @@
+//
+//  UIFont+LatoFonts.h
+//  Festivalama
+//
+//  Created by Sztanyi Szabolcs on 17/06/15.
+//  Copyright (c) 2015 Sztanyi Szabolcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFont (LatoFonts)
+
+@end
---
+++ Festivalama/Festivalama/custom objects/UIFont+LatoFonts.m
@@ -0,0 +1,13 @@
+//
+//  UIFont+LatoFonts.m
+//  Festivalama
+//
+//  Created by Sztanyi Szabolcs on 17/06/15.
+//  Copyright (c) 2015 Sztanyi Szabolcs. All rights reserved.
+//
+
+#import "UIFont+LatoFonts.h"
+
+@implementation UIFont (LatoFonts)
+
+@end
