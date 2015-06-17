864a87f334ab76d27ad7974de7a083ce96f79af0
Szabolcs Sztányi <sax155@icloud.com>
2015-06-18 01:34:32 +0200

    added remote notification and rate the app popup

--- Festivalama/Festivalama/AppDelegate.m
+++ Festivalama/Festivalama/AppDelegate.m
@@ -14,9 +14,15 @@
 #import "PopupView.h"
 #import "CoreDataHandler.h"
 
+#define IS_iOS8 [[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0
+
 @interface AppDelegate () <PopupViewDelegate>
 @property (nonatomic, strong) Reachability *internetReachable;
 @property (nonatomic, strong) PopupView *noInternetPopup;
+@property (nonatomic, strong) PopupView *onTrackPopup;
+@property (nonatomic, strong) PopupView *reviewInAppStorePopup;
+
+@property (nonatomic, strong) NSTimer *popupDisplayerTimer;
 @end
 
 @implementation AppDelegate
@@ -47,6 +53,77 @@
     [[CoreDataHandler sharedHandler] saveMasterContext];
 }
 
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+
+}
+
+#pragma mark - popup view handling
+- (void)checkToDisplayOnTrackPopup
+{
+    if ([GeneralSettings passedIntervalSinceAppStart] > (2 * 60) && ![GeneralSettings wasOnTrackPromptShown]) {
+        [self displayOnTrackPopup];
+    } else if ([GeneralSettings passedIntervalSinceAppStart] > (5 * 60) && ![GeneralSettings wasRateAppShown]) {
+        [self displayAppStoreReviewPopup];
+        [self.popupDisplayerTimer invalidate];
+        self.popupDisplayerTimer = nil;
+    }
+}
+
+- (void)startPopupTimer
+{
+    if ([GeneralSettings wasOnTrackPromptShown] || [GeneralSettings wasRateAppShown]) {
+        return;
+    }
+    self.popupDisplayerTimer = [NSTimer scheduledTimerWithTimeInterval:10.0 target:self selector:@selector(updatePopupTimer:) userInfo:nil repeats:YES];
+}
+
+- (void)updatePopupTimer:(NSTimer*)timer
+{
+    [self checkToDisplayOnTrackPopup];
+}
+
+- (void)displayOnTrackPopup
+{
+    if (self.onTrackPopup.superview) {
+        return;
+    }
+    self.onTrackPopup = [[PopupView alloc] initWithDelegate:self];
+    [self.onTrackPopup setupWithConfirmButtonTitle:@"Klar, warum nicht"
+                                 cancelButtonTitle:nil
+                                         viewTitle:@"On Track"
+                                              text:@"Sicher Dir die Möglichkeit an geschlossenen Vorverkaufs Aktionen teilzunehmen."
+                                              icon:[UIImage imageNamed:@"iconOnTrack"]];
+    [self.onTrackPopup showPopupViewAnimationOnView:self.window withBlurredBackground:YES];
+    [GeneralSettings setOnTrackPromptWasShown];
+}
+
+- (void)displayAppStoreReviewPopup
+{
+    if (self.reviewInAppStorePopup.superview) {
+        return;
+    }
+    self.reviewInAppStorePopup = [[PopupView alloc] initWithDelegate:self];
+    [self.reviewInAppStorePopup setupWithConfirmButtonTitle:@"Klar, warum nicht"
+                                          cancelButtonTitle:@"Vielleicht später"
+                                                  viewTitle:@"Deine ideen"
+                                                       text:@"Hast Du Ideen was man an der App verbessern könnte? Wir würden Sie gerne hören. Über den App Store kannst Du uns Deinen Kommentar hinterlassen"
+                                                       icon:[UIImage imageNamed:@"iconStar"]];
+    [self.reviewInAppStorePopup showPopupViewAnimationOnView:self.window withBlurredBackground:YES];
+    [GeneralSettings setRateAppWasShown];
+}
+
+#pragma mark - internet connection checking methods
 - (void)addNetworkObserver
 {
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(networkStatusChanged:) name:kReachabilityChangedNotification object:nil];
@@ -89,10 +166,17 @@
     self.noInternetPopup = nil;
 }
 
-- (void)popupViewConfirmButtonPressed
+- (void)popupViewConfirmButtonPressed:(PopupView *)popupView
 {
-    [self.noInternetPopup dismissViewWithAnimation:YES];
-    self.noInternetPopup = nil;
+    if (popupView == self.onTrackPopup) {
+//        if (IS_iOS8) {
+//            [[UIApplication sharedApplication] registerForRemoteNotifications];
+//        } else {
+            [[UIApplication sharedApplication] registerForRemoteNotificationTypes:UIRemoteNotificationTypeAlert|UIRemoteNotificationTypeBadge|UIRemoteNotificationTypeSound];
+//        }
+    }
+    [popupView dismissViewWithAnimation:YES];
+    popupView = nil;
 
     if ([self.internetReachable currentReachabilityStatus] == NotReachable) {
         [self showNoInternetPopup];
